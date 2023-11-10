06bf528fc859acf9bea804cb72febfd39a4815c2
Daniil Skuratov <skuratov.danya@gmail.com>
2023-11-10 19:28:24 +0300

    main

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ TOKEN = getenv("6480715477:AAF0w8AQeVTVepQ2XtKMEwiEuFcd2iafHno")
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
@@ -23,28 +24,6 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
-
-async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    if __name__ == "__main__":
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+        asyncio.run(main())
