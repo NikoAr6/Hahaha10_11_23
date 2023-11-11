62f986d08a0875b40c026ead33ceb81905ec8321
Daniil Skuratov <skuratov.danya@gmail.com>
2023-11-11 11:32:59 +0300

    бот отправляет сообщения

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import telebot
-import utils
 from telebot import types  # для указание типов
-bot = telebot.TeleBot("6480715477:AAF0w8AQeVTVepQ2XtKMEwiEuFcd2iafHno")
 
+bot = telebot.TeleBot("6480715477:AAF0w8AQeVTVepQ2XtKMEwiEuFcd2iafHno")
+bot.set_webhook()
+import sqlite3
+
+# Создаем или подключаемся к базе данных
+conn = sqlite3.connect('users.db', check_same_thread=False)
+cursor = conn.cursor()
+
+# Создаем таблицу, если она не существует
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+               (id INTEGER PRIMARY KEY, username TEXT)''')
+def add_user_to_db(user_id, username):
+    cursor.execute('INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)', (user_id, username))
+    conn.commit()
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -12,21 +24,31 @@ def start(message):
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id,
                      text="Привет, {0.first_name}! Я бот для обучения, введите имя".format(
-                         message.from_user))
-    def name():
-        pass #заносится в базу данных
+                         message.from_user), reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def name(message):
+    user_id = message.from_user.id
+    username = message.text
+    add_user_to_db(user_id, username)
+    bot.send_message(message.chat.id, username)
+
 @bot.message_handler(content_types=['text'])
 def func(message):
-    if (message.text == "Посмотреть рейтинг"):
-        bot.send_message(message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
-        def see():
-            pass
+    if (message.text == "Поздороваться"):
+        bot.send_message(message.chat.id, text="Привеt")
+        def func(message):
+            if (message.text == "Посмотреть рейтинг"):
+                bot.send_message(message.chat.id, text="Привет")
 
-    elif (message.text == "Начать тест"):
-        def test():
-            pass
-    elif (message.text == "Мои данные"):
-        def data():
-            pass
+                def see():
+                    pass
+
+            elif (message.text == "Начать тест"):
+                def test():
+                    pass
+            elif (message.text == "Мои данные"):
+                def data():
+                    pass
 
 bot.polling(none_stop=True)
--- utils.py
+++ utils.py
@@ -4,3 +4,35 @@ def tes():
     pass
 def name():
     pass
+import telebot
+import utils
+from telebot import types  # для указание типов
+bot = telebot.TeleBot("6480715477:AAF0w8AQeVTVepQ2XtKMEwiEuFcd2iafHno")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Посмотреть рейтинг")
+    btn2 = types.KeyboardButton("Начать тест")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id,
+                     text="Привет, {0.first_name}! Я бот для обучения, введите имя".format(
+                         message.from_user))
+    def name():
+        pass #заносится в базу данных
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if (message.text == "Посмотреть рейтинг"):
+        bot.send_message(message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
+        def see():
+            pass
+
+    elif (message.text == "Начать тест"):
+        def test():
+            pass
+    elif (message.text == "Мои данные"):
+        def data():
+            pass
+
+bot.polling(none_stop=True)
